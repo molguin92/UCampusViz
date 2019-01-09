daf1dcaeaf27a0561e49dceaaccf2539e36a13a2
Manuel Osvaldo Olguín <molguin@kth.se>
2019-01-09 19:24:36 +0100

    fjnb

--- plot.js
+++ plot.js
@@ -1,19 +1,17 @@
-d3.select("body").append("svg");
-d3.json('https://raw.githubusercontent.com/molguin92/UCampusParser/master/graph.json', function (data) {
-    console.log(data); // this is your data
-    nodes = data.nodes;
-    links = data.links;
+const width = window.innerWidth;
+const height = window.innerHeight;
 
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+function getNodeColor(node) {
+    return 'red';
+}
+
+d3.json('https://raw.githubusercontent.com/molguin92/UCampusParser/master/graph.json').then(function (data) {
+
+    d3.select('body').append('svg');
 
     const svg = d3.select('svg')
         .attr('width', width)
         .attr('height', height);
-
-    function getNodeColor(node) {
-        return node.level === 1 ? 'red' : 'gray'
-    }
 
     const simulation = d3.forceSimulation()
         .force('charge', d3.forceManyBody().strength(-20))
@@ -21,14 +19,14 @@ d3.json('https://raw.githubusercontent.com/molguin92/UCampusParser/master/graph.
 
     const nodeElements = svg.append('g')
         .selectAll('circle')
-        .data(nodes)
+        .data(data.nodes)
         .enter().append('circle')
         .attr('r', 10)
         .attr('fill', getNodeColor);
 
     const textElements = svg.append('g')
         .selectAll('text')
-        .data(nodes)
+        .data(data.nodes)
         .enter().append('text')
         .text(node => node.label)
         .attr('font-size', 15)
