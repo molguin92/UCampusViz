1b5c6a8c1f8c789be6fe848c60affe4ad2f15c58
Manuel Olguin <manuel@olguin.se>
2019-01-10 12:32:35 +0100

    graph

--- plot.js
+++ plot.js
@@ -1,35 +1,85 @@
-const width = window.innerWidth;
-const height = window.innerHeight;
+const width = 0.95 * window.innerWidth,
+    height = 0.95 * window.innerHeight;
 
-function getNodeColor(node) {
-    return 'red';
-}
+const offset_top = (window.innerHeight - height) / 2,
+    offset_side = (window.innerWidth - width) / 2;
 
-d3.json('https://raw.githubusercontent.com/molguin92/UCampusParser/master/graph.json').then(function (data) {
-
-    d3.select('body').append('svg');
-
-    const svg = d3.select('svg')
-        .attr('width', width)
-        .attr('height', height);
-
-    const simulation = d3.forceSimulation()
-        .force('charge', d3.forceManyBody().strength(-20))
-        .force('center', d3.forceCenter(width / 2, height / 2));
-
-    const nodeElements = svg.append('g')
-        .selectAll('circle')
-        .data(data.nodes)
-        .enter().append('circle')
-        .attr('r', 10)
-        .attr('fill', getNodeColor);
-
-    const textElements = svg.append('g')
-        .selectAll('text')
-        .data(data.nodes)
-        .enter().append('text')
-        .text(node => node.label)
-        .attr('font-size', 15)
-        .attr('dx', 15)
-        .attr('dy', 4);
+const zoom = d3.zoom().on('zoom', function () {
 });
+
+const svg = d3.select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append("g")
+    .attr("transform", "translate(" + offset_top + "," + offset_side + ")");
+
+const rect = svg.append("rect")
+    .attr("width", width * 5)
+    .attr("height", height * 5)
+    .style("fill", "none")
+    .style("pointer-events", "all");
+
+const linkforce = d3.forceLink()
+    .id(function (link) {
+        return link.id;
+    })
+    .strength(1);
+
+const simulation = d3.forceSimulation()
+    .force('link', linkforce)
+    .force('charge', d3.forceManyBody().strength(-20))
+    .force('center', d3.forceCenter(width / 2, height / 2));
+
+const container = svg.append("g");
+
+d3.json('https://raw.githubusercontent.com/molguin92/UCampusParser/master/graph.json')
+    .then(function (data) {
+
+        const links = container.append('g')
+            .selectAll('line')
+            .data(data.links)
+            .enter().append('line')
+            .attr('stroke-width', 1)
+            .attr('stroke', '#E5E5E5');
+
+        const nodes = container.append("g")
+            .attr("class", "dot")
+            .selectAll("circle")
+            .data(data.nodes)
+            .enter().append("circle")
+            .attr('id', function (d) {
+                return d.id;
+            })
+            .attr("r", 5)
+            .attr("cx", function (d) {
+                return d.x;
+            })
+            .attr("cy", function (d) {
+                return d.y;
+            });
+
+        simulation.nodes(data.nodes).on('tick', () => {
+                nodes
+                    .attr('cx',
+                        node => node.x
+                    )
+                    .attr('cy',
+                        node => node.y
+                    )
+                    .attr('x',
+                        node => node.x
+                    )
+                    .attr('y',
+                        node => node.y
+                    );
+
+                links.attr('x1', link => link.source.x)
+                    .attr('y1', link => link.source.y)
+                    .attr('x2', link => link.target.x)
+                    .attr('y2', link => link.target.y);
+
+                simulation.force("link").links(data.links);
+            }
+        );
+    });
