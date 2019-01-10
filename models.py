adeb4f2a8ad3c4aa2c033251a852a5614df21ed5
Manuel Olguin <manuel@olguin.se>
2019-01-10 12:52:37 +0100

    add dept to data

--- models.py
+++ models.py
@@ -2,12 +2,11 @@ from typing import List
 
 
 class Course:
-    def __init__(self, id: str, name: str, credits: int,
+    def __init__(self, id: str, name: str, credits: int, dept: str,
                  depends: List[str], equivs: List[str]):
         self.id = id
         self.name = name
+        self.dept = dept
         self.credits = credits
         self.depends = depends
         self.equivs = equivs
-
-
--- parser.py
+++ parser.py
@@ -14,30 +14,31 @@ CATALOG_DIR = './catalog'
 BASE_URL = 'https://ucampus.uchile.cl/m/fcfm_catalogo/'
 YEAR_RANGE = range(2013, 2019)
 DEPTS = {
-    12060003: 'AA - Área para el Aprendizaje de la Ingeniería y Ciencias A2IC',
-    3       : 'AS - Departamento de Astronomía',
-    5       : 'CC - Departamento de Ciencias de la Computación',
-    6       : 'CI - Departamento de Ingeniería Civil',
-    306     : 'CM - Departamento de Ciencia de los Materiales',
-    7       : 'DR - Área de Deportes, Recreación y Cultura',
-    305     : 'ED - Doctorado en Ingeniería Eléctrica',
-    8       : 'EH - Área de Estudios Humanísticos',
-    9       : 'EI - Área de Idiomas, Escuela de Ingeniería',
-    12060002: 'EI - Área de Ingeniería e Innovación',
-    27      : 'EI - Escuela de Ingeniería',
-    10      : 'EL - Departamento de Ingeniería Eléctrica',
-    303     : 'EP - Escuela de Postgrado',
-    12      : 'ES - Escuela de Ingeniería y Ciencias',
-    310     : 'FG - Plataforma',
-    13      : 'FI - Departamento de Física',
-    15      : 'GF - Departamento de Geofísica',
-    16      : 'GL - Departamento de Geología',
-    19      : 'IN - Departamento de Ingeniería Industrial',
-    21      : 'MA - Departamento de Ingeniería Matemática',
-    22      : 'ME - Departamento de Ingeniería Mecánica',
-    23      : 'MI - Departamento de Ingeniería de Minas',
-    24      : 'MT - Doctorado en Ciencia de los Materiales',
-    307     : 'QB - Departamento de Ingeniería Química y Biotecnología'
+    12060003: ('AA',
+               'Área para el Aprendizaje de la Ingeniería y Ciencias A2IC'),
+    3       : ('AS', 'Departamento de Astronomía'),
+    5       : ('CC', 'Departamento de Ciencias de la Computación'),
+    6       : ('CI', 'Departamento de Ingeniería Civil'),
+    306     : ('CM', 'Departamento de Ciencia de los Materiales'),
+    7       : ('DR', 'Área de Deportes, Recreación y Cultura'),
+    305     : ('ED', 'Doctorado en Ingeniería Eléctrica'),
+    8       : ('EH', 'Área de Estudios Humanísticos'),
+    9       : ('EI', 'Área de Idiomas, Escuela de Ingeniería'),
+    12060002: ('EI', 'Área de Ingeniería e Innovación'),
+    27      : ('EI', 'Escuela de Ingeniería'),
+    10      : ('EL', 'Departamento de Ingeniería Eléctrica'),
+    303     : ('EP', 'Escuela de Postgrado'),
+    12      : ('ES', 'Escuela de Ingeniería y Ciencias'),
+    310     : ('FG', 'Plataforma'),
+    13      : ('FI', 'Departamento de Física'),
+    15      : ('GF', 'Departamento de Geofísica'),
+    16      : ('GL', 'Departamento de Geología'),
+    19      : ('IN', 'Departamento de Ingeniería Industrial'),
+    21      : ('MA', 'Departamento de Ingeniería Matemática'),
+    22      : ('ME', 'Departamento de Ingeniería Mecánica'),
+    23      : ('MI', 'Departamento de Ingeniería de Minas'),
+    24      : ('MT', 'Doctorado en Ciencia de los Materiales'),
+    307     : ('QB', 'Departamento de Ingeniería Química y Biotecnología')
 }
 
 
@@ -51,7 +52,7 @@ class CourseParser(HTMLParser):
 
 
 def fetch_html():
-    for dept_id, dept_name in DEPTS.items():
+    for dept_id, (dept_cod, dept_name) in DEPTS.items():
         for year in YEAR_RANGE:
             for sem in (1, 2):
                 params = {'semestre': year * 10 + sem, 'depto': dept_id}
@@ -73,7 +74,8 @@ def course_str_to_list(course_list):
         lst))
 
 
-def parse_courses(year: int, semester: int, dept: int) -> List[Course]:
+def parse_courses(year: int, semester: int, dept: int, dept_id: str) \
+        -> List[Course]:
     if year not in YEAR_RANGE or semester not in (1, 2):
         raise RuntimeError('Year or semester out of range!\n'
                            f'Year {year}, Semester {semester}')
@@ -111,7 +113,7 @@ def parse_courses(year: int, semester: int, dept: int) -> List[Course]:
             if eqs:
                 eqs = course_str_to_list(eqs)
 
-            courses.append(Course(r_id, r_name, uds, reqs, eqs))
+            courses.append(Course(r_id, r_name, uds, dept_id, reqs, eqs))
 
     return courses
 
@@ -120,8 +122,8 @@ if __name__ == '__main__':
     courses = {}
     for year in YEAR_RANGE:
         for sem in (1, 2):
-            for dept in DEPTS.keys():
-                for c in parse_courses(year, sem, dept):
+            for dept, (dept_id, name) in DEPTS.items():
+                for c in parse_courses(year, sem, dept, dept_id):
                     courses[c.id] = c
 
     G = nx.DiGraph()
@@ -129,9 +131,10 @@ if __name__ == '__main__':
 
     for c_id, course in courses.items():
         G.nodes[c_id]['label'] = c_id
+        G.nodes[c_id]['dept'] = course.dept
         for req in course.depends:
             G.add_edge(req, c_id)
 
-# %%
+    # %%
     with open('./graph.json', 'w') as out_f:
         out_f.write(json.dumps(nx.node_link_data(G)))
